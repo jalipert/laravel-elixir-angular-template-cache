82aac92783d6e55c187f9dc5f086aaf4dabdd9d2
Larkin Whitaker <larkin@keystrokecreative.com>
2015-10-12 00:28:27 -0400

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var gulp 	   = require('gulp'),
-	elixir 	   = require('laravel-elixir'),
-	ext 	   = require('gulp-ext-replace'),
-	uglify 	   = require('gulp-uglify'),
-	sourcemaps = require('gulp-sourcemaps');
-
+var elixir 		  = require('laravel-elixir'),
+	gulp 		  = require("gulp"),
+	templateCache = require('gulp-angular-templatecache'),
+	insert 		  = require('gulp-insert');
 
 var Task = elixir.Task;
 
-
-elixir.extend('uglify', function(sourceFile, outputFolder) 
-{ 
-	new Task('uglify', function() 
+elixir.extend("angulartemplatecache", function(options, from, to, wrap) 
+{
+	new Task('angulartemplatecache', function() 
 	{
-        return gulp.src(sourceFile)
-        			// Initialize
-        		   .pipe(sourcemaps.init())
-		           .pipe(uglify())
-		           // Add .min extension to file
-		           .pipe(ext('.min.js'))
-		           // Output minified js
-		           .pipe(gulp.dest(outputFolder))
-		           // Write and Output minified js mapping file
-		           .pipe(sourcemaps.write('./'))
-				   .pipe(gulp.dest(outputFolder));
-    });
+	    if (wrap)
+	    {
+	        gulp.src(from)
+	            .pipe(templateCache(options))
+	            .pipe(insert.wrap('(function(angular) {', '})(angular);'))
+	            .pipe(gulp.dest(to));
+	    } 
+	    else
+	    {
+	        gulp.src(from)
+	            .pipe(templateCache(options))
+	            .pipe(gulp.dest(to));
+	    }
+    })
+    .watch(from);
+
 });
